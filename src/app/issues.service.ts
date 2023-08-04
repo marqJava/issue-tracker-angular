dc060c97c33029a2d00a824a5cbbd5f58535c0de
unknown <marq.java@gmail.com>
2023-08-03 23:30:06 -0500

    Tasklist Angular project first version

---
+++ src/app/issues.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Issue } from './issue';
+//if we want to import mock data==> import { issues } from '../assets/mock-issues';
+import { issues } from '../assets/mock-issues';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IssuesService {
+  private issues: Issue[] = [];
+
+  constructor() {
+    this.issues = issues;
+  }
+
+  getPendingIssues(): Issue[] {
+    return this.issues.filter((issue) => !issue.completed);
+  }
+
+  createIssue(issue: Issue) {
+    issue.issueNo = this.issues.length + 1;
+    this.issues.push(issue);
+  }
+
+  completeIssue(issue: Issue) {
+    const selectedIssue: Issue = {
+      ...issue,
+      completed: new Date(),
+    };
+    const index = this.issues.findIndex((i) => i === issue);
+    this.issues[index] = selectedIssue;
+  }
+
+  getSuggestions(title: string): Issue[] {
+    if (title.length > 3) {
+      return this.issues.filter((issue) => issue.title.indexOf(title) !== - 1);
+    }
+    return [];
+  }
+}
